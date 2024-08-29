28d97a04fe58282678adc288ca2f472c219c9cba
juniorsir <140545024+juniorsir@users.noreply.github.com>
2024-08-29 22:31:02 +0530

    Create dl.sh

---
+++ dl.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+# Colors
+# ----------------------------------------
+BL='\e[01;90m' > /dev/null 2>&1; # Black
+R='\e[01;91m' > /dev/null 2>&1; # Red
+G='\e[01;92m' > /dev/null 2>&1; # Green
+Y='\e[01;93m' > /dev/null 2>&1; # Yellow
+B='\e[01;0m' > /dev/null 2>&1; # Blue
+P='\e[01;95m' > /dev/null 2>&1; # Purple
+C='\e[01;96m' > /dev/null 2>&1; # Cyan
+W='\e[01;0m' > /dev/null 2>&1; # White
+LG='\e[01;37m' > /dev/null 2>&1; # Light Gray
+N='\e[0m' > /dev/null 2>&1; # Null
+L='\033[7m' > /dev/null 2>&1; #Lines
+X='\033[0m' > /dev/null 2>&1; #Closer
+# ----------------------------------------
+
+clear
+
+# Function to display options
+show_options() {
+    echo ""
+echo -e $R"          _  _   _   _   _   _  "$N
+echo -e $R"      _ /   / \ / \ / \ / \ / \ "$N
+echo -e $R"      _    ( + | S | I | R | + )"$N
+echo -e $R"        \ _ \_/ \_/ \_/ \_/ \_/ "$N
+
+echo -e $C "╔════════════════════════════════════════╗"
+echo -e $G "║ ==> Project Name  :Termux-YDL           ║"
+echo -e $G "║ ==> Author        :JuniorSir            ║"
+echo -e $G "║ ==> Github                              ║"
+echo -e $G "║ ==> Telegram      :juniorsir_bot        ║"
+echo -e $C "╠════════════════════════════════════════╝"
+echo -e $C "╠═▶ [ Select Format:: ]"
+echo -e $G "╠═▶ [1] Music Mp3♫"
+echo -e $G "╠═▶ [2] Video 360p"
+echo -e $G "╠═▶ [3] Video 480p"
+echo -e $G "╠═▶ [4] Video 720p"
+echo -e $G "╠═▶ [5] Video 1080p"
+echo -e $G "╠═▶ [6] Video 2160p"
+echo -e $G "╠═▶ [7] Play Background"
+echo -e $G "╠═▶ [8] Exit Termux-YDL"
+}
+
+
+# Read the URL from the user
+echo -e $G
+ read -p  " Enter the video URL:" URL
+
+# Loop until the user chooses to exit
+while true; do
+    show_options
+    read -p "Enter your choice [1-8]: " choice
+
+
+      case $choice in
+
+        1)
+            echo "-x --no-mtime -o /data/data/com.termux/files/home/storage/YouTube/%(title)s.%(ext)s -f \"bestaudio\" --extract-audio --audio-format mp3 --audio-quality 0" > ~/.config/yt-dlp/config
+            yt-dlp $1
+            echo -e $G"\n   Finished...\n"$N
+            sleep 1
+            exit
+        ;;
+
+        2)
+
+            echo "--no-mtime -o /data/data/com.termux/files/home/storage/YouTube/%(title)s.%(ext)s -f \"bestvideo[height<=360][ext=mp4]+bestaudio[ext=m4a]\"" > ~/.config/yt-dlp/config
+            yt-dlp $1
+            echo -e $G"\n   Finished...\n"$N
+            sleep 1
+            exit
+        ;;
+
+        3)
+            echo "--no-mtime -o /data/data/com.termux/files/home/storage/YouTube/%(title)s.%(ext)s -f \"bestvideo[height<=480][ext=mp4]+bestaudio[ext=m4a]\"" > ~/.config/yt-dlp/config
+            yt-dlp $1
+            echo -e $G"\n   Finished...\n"$N
+            sleep 1
+            exit
+        ;;
+        4)
+            echo "--no-mtime -o /data/data/com.termux/files/home/storage/YouTube/%(title)s.%(ext)s -f \"bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]\"" > ~/.config/yt-dlp/config
+            yt-dlp $1
+            echo -e $G"\n   Finished...\n"$N
+            sleep 1
+            exit
+        ;;
+
+        5)
+            echo "--no-mtime -o /data/data/com.termux/files/home/storage/YouTube/%(title)s.%(ext)s -f \"bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]\"" > ~/.config/yt-dlp/config
+            yt-dlp $1
+            echo -e $G"\n   Finished...\n"$N
+            sleep 1
+            exit
+        ;;
+
+        6)
+            echo "--no-mtime -o /data/data/com.termux/files/home/storage/YouTube/%(title)s.%(ext)s -f \"bestvideo[height<=2160][ext=mp4]+bestaudio[ext=m4a]\"" > ~/.config/yt-dlp/config
+            yt-dlp $1
+            echo -e $G"\n   Finished...\n"$N
+            sleep 1
+            exit
+        ;;
+
+        7)
+            echo "--no-mtime -o /data/data/com.termux/files/home/storage/YouTube/%(title)s.%(ext)s -f \"bestvideo[height<=2160][ext=mp4]+bestaudio[ext=m4a]\"" > ~/.config/yt-dlp/config
+            yt-dlp -o - | mpv - $1
+            echo -e $G"\n   Finished...\n"$N
+            sleep 1
+            exit
+        ;;
+
+        8)
+          echo -e $C "Exiting.."
+            exit 0
+            ;;
+        *)
+            echo "Invalid option. Please choose a number between 1 and 8."
+            ;;
+    esac
+done
+
+
+
+
